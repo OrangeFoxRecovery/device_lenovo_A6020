d9e3bbf33d19327a320e4888733a45827a9ec8a8
Sushrut1101 <guptasushrut@gmail.com>
2022-11-11 01:42:02 +0530

    A6020: EOL Update
    fox_9.0 is deprecated since 1st November 2022

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -63,6 +63,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export FOX_VERSION="R11.1_1"
     export OF_MAINTAINER_AVATAR="$INCLUDE_PATH/Sushrut1101.png"
     export FOX_BUILD_TYPE="Stable"
+    export FOX_VARIANT="EOL"
 
 	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
 	export FOX_USE_BASH_SHELL=1
